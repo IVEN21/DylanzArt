9993d84944405c54d709400bcfe6d939f4356f3f
Your Name <zhanghaoxian8@gmail.com>
2021-01-24 18:59:30 -0500

    major change

--- src/Components/Drawing.jsx
+++ src/Components/Drawing.jsx
@@ -1,67 +1,94 @@
+// import React, { useEffect, useState } from "react";
+// import { animated, useTransition } from "react-spring";
+// function Drawing({ drawing, setSortName, id }) {
+//   const [info, setInfo] = useState(false);
+//   const [height, setHeight] = useState(null);
+//   const [width, setWidth] = useState(null);
+
+//   const info_animation = useTransition(info, null, {
+//     from: { transform: `translate3d(0,200px,0)`, opacity: 0 },
+//     enter: { transform: `translate3d(0,0,0)`, opacity: 1 },
+//     leave: { transform: `translate3d(0,200px,0)`, opacity: 0 },
+//     config: {
+//       friction: 500,
+//       tension: 3200,
+//     },
+//   });
+
+//   return (
+//     <div
+//       key={id}
+//       className="drawing"
+//       style={{
+//         backgroundImage: `url(${drawing.url})`,
+//         height: height,
+//         width: width,
+//       }}
+//       onMouseEnter={() => setInfo(true)}
+//       onMouseLeave={() => setInfo(false)}
+//     >
+//       {info_animation.map(
+//         ({ item, props, key }) =>
+//           item && (
+//             <animated.div className="info" style={props} key={key}>
+//               {drawing.tags.map((tags) => (
+//                 <span
+//                   onClick={() => {
+//                     setSortName(tags.tag);
+//                   }}
+//                   key={tags._id}
+//                 >
+//                   {tags.tag}
+//                 </span>
+//               ))}
+//             </animated.div>
+//           )
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Drawing;
 import React, { useEffect, useState } from "react";
 import { animated, useTransition } from "react-spring";
+
 function Drawing({ drawing, setSortName, id }) {
-  const [info, setInfo] = useState(false);
-  const [height, setHeight] = useState(null);
-  const [width, setWidth] = useState(null);
- 
-  useEffect(() => {
+  const [height, setHeight] = useState(300);
+  const [width, setWidth] = useState(300);
+
+  function changeSize() {
     const img = new Image();
     img.src = drawing.url;
 
     img.onload = () => {
-      if (img.width > img.height) {
-        const scale = img.width / img.height;
-        setHeight(400 / scale);
-        setWidth(400);
+      if (img.height > img.width) {
+        setHeight((img.height / img.width) * 300);
       } else {
-        const scale = img.height / img.width;
-        setHeight(400);
-        setWidth(400 / scale);
+        setWidth((img.width / img.height) * 300);
       }
     };
-  });
-
-  const info_animation = useTransition(info, null, {
-    from: { transform: `translate3d(0,200px,0)`, opacity: 0 },
-    enter: { transform: `translate3d(0,0,0)`, opacity: 1 },
-    leave: { transform: `translate3d(0,200px,0)`, opacity: 0 },
-    config: {
-      friction: 500,
-      tension: 3000,
-    },
-  });
+  }
 
   return (
-    <div
-      key={id}
-      className="drawing"
-      style={{
-        backgroundImage: `url(${drawing.url})`,
-        height: height,
-        width: width,
-      }}
-      onMouseEnter={() => setInfo(true)}
-      onMouseLeave={() => setInfo(false)}
-
-    >
-      {info_animation.map(
-        ({ item, props ,key}) =>
-          item && (
-            <animated.div className="info" style={props} key={key}>
-              {drawing.tags.map((tags) => (
-                <span
-                  onClick={() => {
-                    setSortName(tags.tag);
-                  }}
-                  key={tags._id}
-                >
-                  {tags.tag}
-                </span>
-              ))}
-            </animated.div>
-          )
-      )}
+    <div className="drawings_container">
+      <a
+        className="drawings"
+        style={{ height, width, backgroundImage: `url(${drawing.url})` }}
+        onMouseEnter={changeSize}
+        onMouseLeave={() => {
+          setHeight(300);
+          setWidth(300);
+        }}
+        onClick={() => {}}
+        href={drawing.url}
+        target="_blank"
+      >
+        <div className="info">
+          {drawing.tags.map((tags) => (
+            <span key={tags._id}>{tags.tag}</span>
+          ))}
+        </div>
+      </a>
     </div>
   );
 }
--- src/Components/Main_5.jsx
+++ src/Components/Main_5.jsx
@@ -4,92 +4,95 @@ import { faGrinHearts } from "@fortawesome/free-solid-svg-icons";
 import Drawing from "./Drawing";
 import { getDrawings } from "./BackendService/dbService";
 import { toast } from "react-toastify";
-import SearchBox from "./SearchBox"
+import SearchBox from "./SearchBox";
 class Main_5 extends Component {
   state = {
     sortName: "All",
     drawings: [],
-    query: ""
-  }
+    query: "",
+  };
   async componentDidMount() {
     try {
       const data = await getDrawings();
-      this.setState({ drawings: data })
+      this.setState({ drawings: data });
     } catch (error) {
-      toast.error("Something Wrong With Api Fetching")
+      toast.error("Something Wrong With Api Fetching");
     }
   }
 
   filter_drawings = () => {
-    const { drawings, sortName, query } = this.state
+    const { drawings, sortName, query } = this.state;
     if (query !== "") {
       var temp_drawings = [];
 
       for (var i = 0; i < drawings.length; i++) {
-        loop:
-        for (var j = 0; j < drawings[i].tags.length; j++) {
-          if (drawings[i].tags[j].tag.toLowerCase().includes(query.toLowerCase())) {
-            temp_drawings.push(drawings[i])
+        loop: for (var j = 0; j < drawings[i].tags.length; j++) {
+          if (
+            drawings[i].tags[j].tag.toLowerCase().includes(query.toLowerCase())
+          ) {
+            temp_drawings.push(drawings[i]);
             break loop;
-          };
+          }
         }
       }
       return temp_drawings;
-    }
-    else if (sortName === "All") {
+    } else if (sortName === "All") {
       return drawings;
-    }
-
-    else {
+    } else {
       var temp_drawings = [];
       for (var i = 0; i < drawings.length; i++) {
-        loop:
-        for (var j = 0; j < drawings[i].tags.length; j++) {
-          if (drawings[i].tags[j].tag.toLowerCase().includes(sortName.toLowerCase())) {
-            temp_drawings.push(drawings[i])
+        loop: for (var j = 0; j < drawings[i].tags.length; j++) {
+          if (
+            drawings[i].tags[j].tag
+              .toLowerCase()
+              .includes(sortName.toLowerCase())
+          ) {
+            temp_drawings.push(drawings[i]);
             break loop;
-          };
+          }
         }
       }
       return temp_drawings;
     }
-
   };
   setSortName = (item) => {
-    this.setState({ sortName: item, query: "" })
-  }
+    this.setState({ sortName: item, query: "" });
+  };
   render() {
-
     const categories = () => {
-      const cate = ["All", "Water Color", "Figure", "Oil Painting", "Abstract", "Still-life"];
-      const tempcate = cate.find((item) => item.toLowerCase())
+      const cate = [
+        "All",
+        "Water Color",
+        "Figure",
+        "Oil Painting",
+        "Abstract",
+        "Still-life",
+      ];
+      const tempcate = cate.find((item) => item.toLowerCase());
       if (this.state.query.length > 0 && !tempcate.includes(this.state.query)) {
-        cate.push(this.state.query)
-        return cate
+        cate.push(this.state.query);
+        return cate;
       }
-      if (!cate.includes(this.state.sortName)) cate.push(this.state.sortName)
-      return cate
-
-    }
+      if (!cate.includes(this.state.sortName)) cate.push(this.state.sortName);
+      return cate;
+    };
     //Searchbar Functionality
 
     const onSearch = ({ currentTarget }) => {
-      this.setState({ query: currentTarget.value, sortName: "" })
+      this.setState({ query: currentTarget.value, sortName: "" });
     };
 
     const search_bar = (
       <SearchBox onchange={onSearch} value={this.state.query} />
     );
 
-
     return (
-
       <div className="Main_5">
         <div className="sort">
           <div style={{ width: "100%", textAlign: "center", color: "#f2e7da" }}>
             <div style={{ fontSize: "50px", marginBottom: "50px" }}>
               Gallery
-           {search_bar}
+              {search_bar}
             </div>
           </div>
           <br />
@@ -97,30 +100,46 @@ class Main_5 extends Component {
           <div className="cate_div">
             Categories:
             {categories().map((item) => (
-            <label
-              key={item}
-              onClick={() => this.setState({
-                sortName: item, query: ""
-              })}
-              className={item === this.state.sortName || item === this.state.query ? "active" : ""}
-            >
-              {item}
-            </label>
-          ))}
+              <label
+                key={item}
+                onClick={() =>
+                  this.setState({
+                    sortName: item,
+                    query: "",
+                  })
+                }
+                className={
+                  item === this.state.sortName || item === this.state.query
+                    ? "active"
+                    : ""
+                }
+              >
+                {item}
+              </label>
+            ))}
           </div>
         </div>
-        <div className="Gallery" >
+        <div className="Gallery">
           <div className="main_5_container">
-            {this.filter_drawings().length > 0 ? this.filter_drawings().reverse().map((item) =>
-              <Drawing drawing={item} setSortName={this.setSortName} id={item._id} key={item._id} />
-            ) : <h1 style={{ color: "#ff9cac" }}>No Relevant Result found</h1>}
+            {this.filter_drawings().length > 0 ? (
+              this.filter_drawings()
+                .reverse()
+                .map((item) => (
+                  <Drawing
+                    drawing={item}
+                    setSortName={this.setSortName}
+                    id={item._id}
+                    key={item._id}
+                  />
+                ))
+            ) : (
+              <h1 style={{ color: "#ff9cac" }}>No Relevant Result found</h1>
+            )}
           </div>
         </div>
       </div>
-
     );
   }
 }
 
 export default Main_5;
-
